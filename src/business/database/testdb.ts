44f7f74cb735fcdeb97f53d43b98f1b3260953b4
Alexandyva <alexandrakohne11@gmail.com>
2024-10-25 20:18:22 +0200

    003 DataBase Time Update / Changes at Time

--- src/business/database/testdb.ts
+++ src/business/database/testdb.ts
@@ -1,164 +1,171 @@
 import {DatabaseDoorGuard} from "./database";
 import {container} from "tsyringe";
-import {Main} from "../main/main";
 
+import 'reflect-metadata';
+import {Time} from "../tools/time"; // Necessary if using dependency injection libraries like tsyringe
 
 function logTestResult(description: string, result: any): void {
     console.log(`${description}:`, result);
 }
 
-export function dbtest(): void {
-    const newDb = container.resolve(DatabaseDoorGuard);
+export function dbTest(): void {
+    const db = container.resolve(DatabaseDoorGuard);
 
-    // Test for Settings
-    // IMPORTANT!!!! KEYS ARE UNIQUE, SO IF YOU WANNA DO THIS TEST AGAIN THEN USE OTHER KEYS!!!!
-    //const setting1 = { key: 'theme', value: 'dark' };
-    //const setting2 = { key: 'language', value: 'en' };
-    //newDb.insertSetting(setting1);
-    //newDb.insertSetting(setting2);
-    //logTestResult('Inserted setting1', setting1);
-    //logTestResult('Inserted setting2', setting2);
+    const setting1 = { key: 'alexandra', value: 'goth' };
+    const setting2 = { key: 'lola', value: 'cheese' };
+    db.insertSetting(setting1);
+    db.insertSetting(setting2);
+    logTestResult('setting1', setting1);
+    logTestResult('setting2', setting2);
 
-    const retrievedSetting1 = newDb.getSetting('theme');
-    const retrievedSetting2 = newDb.getSetting('language');
-    logTestResult('Retrieved setting1', retrievedSetting1);
-    logTestResult('Retrieved setting2', retrievedSetting2);
+    const getSetting1 = db.getSetting('alexandra');
+    const getSetting2 = db.getSetting('lola');
+    logTestResult('get setting1', getSetting1);
+    logTestResult('get setting2', getSetting2);
 
-    const allSettings = newDb.getSettings();
-    logTestResult('All settings', allSettings);
+    db.deleteSetting('lola');
 
-    // Test for Events
-    const event1 = { type: 'user_login', timestamp: new Date() };
-    const event2 = { type: 'file_upload', timestamp: new Date() };
-    newDb.insertEvent(event1);
-    newDb.insertEvent(event2);
-    logTestResult('Inserted event1', event1);
-    logTestResult('Inserted event2', event2);
+    const allSettings = db.getSettings();
+    logTestResult('settings', allSettings);
 
-    const retrievedEvent1 = newDb.getEvent(1);
-    const retrievedEvent2 = newDb.getEvent(2);
-    logTestResult('Retrieved event1', retrievedEvent1);
-    logTestResult('Retrieved event2', retrievedEvent2);
+    // Events
+    const event1 = { type: 'login', timestamp: new Date() };
+    const event2 = { type: 'logout', timestamp: new Date() };
+    const eventId1 = db.insertEvent(event1);
+    const eventId2 = db.insertEvent(event2);
+    logTestResult('event1 ID', eventId1);
+    logTestResult('event2 ID', eventId2);
 
-    const allEvents = newDb.getEvents();
-    logTestResult('All events', allEvents);
+    const getEvent = db.getEvent(eventId1);
+    logTestResult('get event1', getEvent);
 
-    // Test for Controllers
+    db.deleteEvent(eventId1);
+
+    const allEvents = db.getEvents();
+    logTestResult('events', allEvents);
+
+
+
+
+    // Controllers
     const controller1 = {
-        name: 'Controller 1',
-        timeFrom: new Date(),
-        timeTo: new Date(),
+        name: 'Controller1',
+        timeFrom: new Time(8, 15),
+        timeTo: new Time(17, 45),
         enabled: true,
-        description: 'Test controller 1',
-        inputs: 'input1,input2',
-        outputs: 'output1,output2',
-        conditionsFrom: 5,
-        conditionsTo: 10,
+        description: 'Primary Controller',
+        inputs: 'Input1',
+        outputs: 'Output1',
+        conditionsFrom: 1,
+        conditionsTo: 2,
     };
     const controller2 = {
-        name: 'Controller 2',
-        timeFrom: new Date(),
-        timeTo: new Date(),
+        name: 'Controller2',
+        timeFrom: new Time(9,0),
+        timeTo: new Time(18, 0),
         enabled: false,
-        description: 'Test controller 2',
-        inputs: 'input3,input4',
-        outputs: 'output3,output4',
-        conditionsFrom: 15,
-        conditionsTo: 20,
+        description: 'Secondary Controller',
+        inputs: 'Input2',
+        outputs: 'Output2',
+        conditionsFrom: 2,
+        conditionsTo: 3,
     };
-    newDb.insertController(controller1);
-    newDb.insertController(controller2);
-    logTestResult('Inserted controller1', controller1);
-    logTestResult('Inserted controller2', controller2);
+    const controllerId1 = db.insertController(controller1);
+    const controllerId2 = db.insertController(controller2);
+    logTestResult('controller1 ID', controllerId1);
+    logTestResult('controller2 ID', controllerId2);
 
-    const retrievedController1 = newDb.getController(1);
-    const retrievedController2 = newDb.getController(2);
-    logTestResult('Retrieved controller1', retrievedController1);
-    logTestResult('Retrieved controller2', retrievedController2);
+    const getController = db.getController(controllerId1);
+    logTestResult('get controller1', getController);
 
-    const allControllers = newDb.getControllers();
-    logTestResult('All controllers', allControllers);
+    db.deleteController(controllerId1);
 
-    // Test for Outputs
-    const output1 = {
-        name: 'Output 1',
-        timeFrom: new Date(),
-        timeTo: new Date(),
-        enabled: true,
-        description: 'Test output 1',
-        type: 'digital',
-        settings: 'high frequency',
-        pin: 'A1',
-        repeat: 3,
-        duration: 5000,
-        channel: 'Channel 1',
-        message: 'Output activated',
-    };
-    const output2 = {
-        name: 'Output 2',
-        timeFrom: new Date(),
-        timeTo: new Date(),
-        enabled: false,
-        description: 'Test output 2',
-        type: 'analog',
-        settings: 'low frequency',
-        pin: 'A2',
-        repeat: 5,
-        duration: 3000,
-        channel: 'Channel 2',
-        message: 'Output deactivated',
-    };
-    newDb.insertOutput(output1);
-    newDb.insertOutput(output2);
-    logTestResult('Inserted output1', output1);
-    logTestResult('Inserted output2', output2);
+    const allControllers = db.getControllers();
+    logTestResult('controllers', allControllers);
 
-    const retrievedOutput1 = newDb.getOutput(1);
-    const retrievedOutput2 = newDb.getOutput(2);
-    logTestResult('Retrieved output1', retrievedOutput1);
-    logTestResult('Retrieved output2', retrievedOutput2);
 
-    const allOutputs = newDb.getOutputs();
-    logTestResult('All outputs', allOutputs);
 
-    // Test for Inputs
+
+    // Inputs
     const input1 = {
-        name: 'Input 1',
-        timeFrom: new Date(),
-        timeTo: new Date(),
+        name: 'Input1',
+        timeFrom: new Time(6, 22),
+        timeTo: new Time(14, 14),
         enabled: true,
-        description: 'Test input 1',
-        type: 'sensor',
-        settings: 'sensitivity high',
-        pin: 'B1',
-        channel: 'Channel 1',
-        message: 'Input detected',
+        description: 'Main input',
+        type: 'digital',
+        settings: '{}',
+        pin: 'P1',
+        channel: 'CH1',
+        message: 'Initial state',
     };
     const input2 = {
-        name: 'Input 2',
-        timeFrom: new Date(),
-        timeTo: new Date(),
+        name: 'Input2',
+        timeFrom: new Time(6, 22),
+        timeTo: new Time(14, 14),
         enabled: false,
-        description: 'Test input 2',
-        type: 'switch',
-        settings: 'sensitivity low',
-        pin: 'B2',
-        channel: 'Channel 2',
-        message: 'Input not detected',
+        description: 'Secondary input',
+        type: 'analog',
+        settings: '{}',
+        pin: 'P2',
+        channel: 'CH2',
+        message: 'Secondary state',
     };
-    newDb.insertInput(input1);
-    newDb.insertInput(input2);
-    logTestResult('Inserted input1', input1);
-    logTestResult('Inserted input2', input2);
+    const inputId1 = db.insertInput(input1);
+    const inputId2 = db.insertInput(input2);
+    logTestResult('input1 ID', inputId1);
+    logTestResult('input2 ID', inputId2);
 
-    const retrievedInput1 = newDb.getInput(1);
-    const retrievedInput2 = newDb.getInput(2);
-    logTestResult('Retrieved input1', retrievedInput1);
-    logTestResult('Retrieved input2', retrievedInput2);
+    const getInput = db.getInput(inputId1);
+    logTestResult('get input1', getInput);
 
-    const allInputs = newDb.getInputs();
-    logTestResult('All inputs', allInputs);
+    db.deleteInput(inputId1);
 
-    // Close the database connection
-    newDb.close();
+    const allInputs = db.getInputs();
+    logTestResult('inputs', allInputs);
+
+    // Outputs
+    const output1 = {
+        name: 'Output1',
+        timeFrom: new Time(6, 22),
+        timeTo: new Time(14, 14),
+        enabled: true,
+        description: 'Primary output',
+        type: 'analog',
+        settings: '{}',
+        pin: 'O1',
+        repeat: 1,
+        duration: 60,
+        channel: 'CH1',
+        message: 'Output active',
+    };
+    const output2 = {
+        name: 'Output2',
+        timeFrom: new Time(6, 22),
+        timeTo: new Time(14, 14),
+        enabled: false,
+        description: 'Backup output',
+        type: 'digital',
+        settings: '{}',
+        pin: 'O2',
+        repeat: 0,
+        duration: 30,
+        channel: 'CH2',
+        message: 'Output inactive',
+    };
+    const outputId1 = db.insertOutput(output1);
+    const outputId2 = db.insertOutput(output2);
+    logTestResult('output1 ID', outputId1);
+    logTestResult('output2 ID', outputId2);
+
+    const getOutput = db.getOutput(outputId1);
+    logTestResult('get output1', getOutput);
+
+    db.deleteOutput(outputId1);
+
+    const allOutputs = db.getOutputs();
+    logTestResult('outputs', allOutputs);
+
+    db.dropAllTables();
+    db.close();
 }
